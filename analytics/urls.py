b40041dc794d1c2ecdb956cb63ac55cf4d62876e
gaji asif <asif01665@yahoo.com>
2025-12-05 12:29:41 +0200

    added performance anlytics api endpoint

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import BlogViewsAPI, TopAnalyticsAPI
+from .views import BlogViewsAPI, TopAnalyticsAPI, PerformanceAnalyticsAPI
 
 urlpatterns = [
     path('blog-views/', BlogViewsAPI.as_view()),
     path('top/', TopAnalyticsAPI.as_view()), 
-    # path('performance/', PerformanceAPI.as_view()),
+    path('performance/', PerformanceAnalyticsAPI.as_view()),
 ]
